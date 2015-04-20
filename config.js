3d8f1e155ecc5aaf772359404beea067675b50fc
郑安然 <1051961997@qq.com>
2015-04-20 16:45:06 +0800

    config.js

---
+++ config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'public': '/Users/ann/Documents/Netease/epay-loan',
+  port: '80',
+  ftl: {
+    base: '/Users/ann/Documents/Netease/epay-loan/ftl',
+    global: {
+ 
+    },
+    'productDetail_000008_new.ftl': function(req, res) {
+      return {
+        saleActivityMap: {
+          "000008": {
+            activityStatus: 'actived'
+          }
+        }
+      }
+    }
+ 
+  },
+  mock: [{
+    url: '/request',
+    method: 'get',
+    status: '200',
+    header: {
+ 
+    },
+    response: function(req, res) {
+      return {
+        a: 1,
+        B: 2
+      }
+    }
+  }],
+  proxy: [{
+    path: '/proxy1',
+    target: 'http://localhost:3000'
+    }
+  ]
+}
